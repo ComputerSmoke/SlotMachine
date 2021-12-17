24296081034d0c538783354eaa41138710f0c313
ComputerSmoke <addison@assurancefamily.com>
2021-12-17 14:58:16 -0600

    Implemented bit shifting to internally use the upper half of IDs for machines, and abstract away tickets and machines storing their ids in the same range of numbers.

--- contracts/Tickets.sol
+++ contracts/Tickets.sol
@@ -5,10 +5,13 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ContextMixin.sol";
 
+//Tickets track slot machine ownership, as well as 
 contract Tickets is ERC1155,ContextMixin,Ownable {
     //OpenSea's ERC1155 Proxy Address - (0x207Fa8Df3a17D96Ca7EA4f2893fcdCb78a304101 for polygon)
     address openSeaProxy;
+    //Each id can only be minted once to prevent changing machines after initial sell.
     mapping(uint256 => bool) exists;
+    //Take the address of OpenSea's ERC1155 Proxy, and the URI where metadata will be served by id.
     constructor(address _openSeaProxy, string memory _URI) ERC1155(_URI) {
         openSeaProxy = _openSeaProxy;
     }
@@ -37,10 +40,18 @@ contract Tickets is ERC1155,ContextMixin,Ownable {
     {
         return ContextMixin.msgSender();
     }
-    //Mint a new machine or ticket. Can only be used once for each machine/ticket type.
-    function mint(uint256 _id, uint256 _amount) external onlyOwner {
+    //Mint a new ticket. Can only be used once for each id.
+    function mintTicket(uint256 _id, uint256 _amount) external onlyOwner {
+        //If ID is in the upper 128 bits, it is too high since we use the upper half for machine ids.
+        require((~_id) & 0xffffffffffffffffffffffffffffffff == 0, "Id is too high.");
         require(!exists[_id], "This id has already been used.");
         _mint(msg.sender, _id, _amount, "");
-    } 
-    
+    }
+    //Mint a new machine. Can only be used once for each id.
+    function mintMachine(uint256 _id) external onlyOwner {
+        //If ID is in the upper 128 bits, it is too high since we use the upper half for machine ids.
+        require((~_id) & 0xffffffffffffffffffffffffffffffff == 0, "Id is too high.");
+        require(!exists[_id], "This id has already been used.");
+        _mint(msg.sender, _id << 128, 1, "");
+    }
 }
